7de93b95bc08b1719453cfa5075147c0a999cc04
YolhaYu <olenka_glushko1993@mail.ru>
2023-02-09 00:40:26 +0300

    fix: change block location

--- src/layout/main.ts
+++ src/layout/main.ts
@@ -3,27 +3,30 @@ import { aside } from '../aside/aside';
 export const mainPage = () => `
       <div class="page__main main-page">
         <div class="main-page__sections">
+          <div class="main-page__animation"></div>
           <section class="main-page__start">
-            <div class="main-page__animation"></div>
-            <h1 class="main-page__h1 main-title">EasyCharts</h1>
-            <h2 class="main-page__h2">Графики/диаграммы онлайн</h2>
-            <button class="main-page__button info-page__button">Cоздать график</button>
-            <div class="main-page__images">
-              <div class="img-block">
-                <img class="main-page__img" src="../assetsStatic/phone.svg" alt="phone">
-                <p class="img-block__text1"><b>Создайте онлайн, бесплатно</b> и без регистрации</p>
-              </div>
-              <div class="img-block">
-                <img class="main-page__img" src="../assetsStatic/share.svg" alt="share">
-                <p class="img-block__text2"><b>Делитесь</b> ссылками на ваши графики</p>
-              </div>
-              <div class="img-block">
-                <img class="main-page__img" src="../assetsStatic/download.svg" alt="download">
-                <p class="img-block__text3"><b>Скачивайте графики</b>
-                  PNG / JPG / GIF / SVG
-                </p>
-              </div>
+            <div class="main-page__start-wrap">
+              <h1 class="main-page__h1 main-title">EasyCharts</h1>
+              <h2 class="main-page__h2">Графики/диаграммы онлайн</h2>
+              <button class="main-page__button info-page__button">Cоздать график</button>
+              <div class="main-page__images">
+                <div class="img-block">
+                  <img class="main-page__img" src="../assetsStatic/phone.svg" alt="phone">
+                  <p class="img-block__text1"><b>Создайте онлайн, бесплатно</b> и без регистрации</p>
+                </div>
+                <div class="img-block">
+                  <img class="main-page__img" src="../assetsStatic/share.svg" alt="share">
+                  <p class="img-block__text2"><b>Делитесь</b> ссылками на ваши графики</p>
+                </div>
+                <div class="img-block">
+                  <img class="main-page__img" src="../assetsStatic/download.svg" alt="download">
+                  <p class="img-block__text3"><b>Скачивайте графики</b>
+                    PNG / JPG / GIF / SVG
+                  </p>
+                </div>
+              </div>            
             </div>
+            ${aside()}
           </section>
           <section class="main-page__gallery">
             <h2 class="main-page__gallery-h2">Галерея графиков/диаграмм</h2>
@@ -43,7 +46,6 @@ export const mainPage = () => `
             </p>
           </section>
         </div>
-        ${aside()}
       </div>
 `;
 
