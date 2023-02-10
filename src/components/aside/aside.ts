74a4c61df0f9e9383ff95e791e3d5bafb6e7c17d
YolhaYu <olenka_glushko1993@mail.ru>
2023-02-10 23:16:33 +0300

    refactor: remove text in html

--- src/components/aside/aside.ts
+++ src/components/aside/aside.ts
@@ -14,44 +14,44 @@ const weather = (): string => `
 
 const currencyConverter = (): string => `
   <div class="currency-converter">
-    <h3 class="currency-converter__h3">Конвертер валют</h3>
-    <p class="currency-converter__p">По курсу НБ РБ</p>
+    <h3 class="currency-converter__h3"></h3>
+    <p class="currency-converter__p"></p>
     <div class="currency-converter__inputs">
       <div class="currency-converter__input">
         <label>BYN
           <input class="converter-input input-byn" type="number">
         </label>
-        <span class="currency-converter__span span-byn">белорусский рубль</span>
+        <span class="currency-converter__span span-byn"></span>
       </div>
       <div class="currency-converter__input">
         <label>USD
           <input class="converter-input input-usd" type="number">
         </label>
-        <span class="currency-converter__span span-usd">доллар США</span>
+        <span class="currency-converter__span span-usd"></span>
       </div>
       <div class="currency-converter__input">
         <label>EUR
           <input class="converter-input input-eur" type="number">
         </label>
-        <span class="currency-converter__span span-eur">евро</span>
+        <span class="currency-converter__span span-eur"></span>
       </div>
       <div class="currency-converter__input">
         <label>RUB
           <input class="converter-input input-rub" type="number">
         </label>
-        <span class="currency-converter__span span-rub">российский рубль</span>
+        <span class="currency-converter__span span-rub"></span>
       </div>
       <div class="currency-converter__input">
         <label>UAH
           <input class="converter-input input-uah" type="number">
         </label>
-        <span class="currency-converter__span span-uah">украинская гривна</span>
+        <span class="currency-converter__span span-uah"></span>
       </div>
       <div class="currency-converter__input">
         <label>PLN
           <input class="converter-input input-pln" type="number">
         </label>
-        <span class="currency-converter__span span-pln">польский злотый</span>
+        <span class="currency-converter__span span-pln"></span>
       </div>
     </div>
   </div>
